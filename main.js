84a434e7d7fdfc39ff88c5453ee130a26d8f027e
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-01-05 17:55:23 +0100

    Random ChuckNorris facts

--- main.js
+++ main.js
@@ -3,13 +3,14 @@ async function getData() {
       const response = await fetch('https://api.chucknorris.io/jokes/random');
       const data = await response.json();
       console.log(data);
-      const jokes = document.querySelector('h1');
+      const jokes = document.querySelector('p');
       jokes.innerHTML = JSON.stringify(data.value);
 
     } catch (error) {
       console.error(error);
     }
   }
+  getData();
 
   const refresh = document.getElementById('refresh');
   console.log(refresh);
