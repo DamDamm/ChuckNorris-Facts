3cd12d5072c3efeb45e9b1f37a93560988057f4b
DamDamm <61273828+DamDamm@users.noreply.github.com>
2023-01-05 20:23:57 +0100

    catégorie fonctionnelle

--- main.js
+++ main.js
@@ -1,8 +1,47 @@
+// Ajout de la fonctionnalité qui permet d'afficher les categories dans la balise Select / Option
+async function getCategories() {
+  try {
+    const select = document.getElementById('categories');
+    const response = await fetch('https://api.chucknorris.io/jokes/categories');
+    const data = await response.json();
+    let options = '';
+    data.forEach(category => {
+      options += `<option>${category}</option>`;
+    });
+    select.innerHTML = options;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getCategories();
+
+
+// Ajout de la fonctionnalité qui permet d'afficher une blague random de l'API par rapport avec la categories selectionnés
+async function getJoke() {
+  try {
+    const select = document.querySelector('option');
+    const category = select.value;
+    const response = await fetch(`https://api.chucknorris.io/jokes/random?category=${category}`);
+    const data = await response.json();
+    const jokePara = document.getElementById('categoriesResult');
+    jokePara.innerHTML = data.value;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getJoke()
+
+
+const categories = document.getElementById('categories');
+categories.addEventListener('click', getJoke);
+
+
+// Ajout de la fonctionnalité qui permet d'afficher une blague random de l'API
 async function getData() {
     try {
       const response = await fetch('https://api.chucknorris.io/jokes/random');
       const data = await response.json();
-      const jokes = document.querySelector('p');
+      const jokes = document.getElementById('randomResult');
       jokes.innerHTML = JSON.stringify(data.value);
 
     } catch (error) {
@@ -12,22 +51,7 @@ async function getData() {
   getData();
 
   const refresh = document.getElementById('refresh');
-  console.log(refresh);
   refresh.addEventListener('click', getData);
 
 
-  async function getCategories() {
-    try {
-      const select = document.getElementById('categories');
-      const response = await fetch('https://api.chucknorris.io/jokes/categories');
-      const data = await response.json();
-      let options = '';
-      data.forEach(category => {
-        options += `<option>${category}</option>`;
-      });
-      select.innerHTML = options;
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  getCategories();
+
